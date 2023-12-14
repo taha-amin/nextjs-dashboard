3671abfe3572efb3ccb70cf795f505f24d7f36d0
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-12-14 12:45:10 -0800

    Finished updating invoice operation by: creating a dynamic route segment with the invoice id. Reading the invoice id from the page params. Fetching the specific invoice from your database. Pre-populating the form with the invoice data. Then updating the invoice data in your database.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,6 +13,8 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+//CREATING INVOICES
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
@@ -28,6 +30,29 @@ export async function createInvoice(formData: FormData) {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+//UPDATING INVOICES
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
 
   revalidatePath('/dashboard/invoices');
